7988436e6f72a90f07db246b575ecd3c1bce7dd8
Jarbas Silva Filho <jarbas.silva.f@gmail.com>
2018-02-18 11:33:44 -0300

    Criando os primeiros testes da classe Jogador

---
+++ Jogador.test.js
@@ -0,0 +1,12 @@
+describe("Jogador", () => {
+
+    it("Ao Iniciar um jogador o mesmo deve ter HP = 100 * lvl", ()=> {
+        let jogador = new Jogador(3, "Petro");
+        expect(jogador.hp).toBe(3 * 100);
+    });
+
+    it("Ao atacar o dano causado por um jogador deve ser igual a 12 * lvl", () => {
+        let jogador = new Jogador(6, "Saiyt");
+        expect(jogador.atacar()).toBe(6 * 12);
+    });
+});
